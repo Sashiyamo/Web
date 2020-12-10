c921adf3f75cc1a985f3eab6a1c9da520ffa4824
Sashiyamo <62176228+Sashiyamo@users.noreply.github.com>
2020-12-10 17:59:06 +0300

    Add files via upload

---
+++ lab6.js
@@ -0,0 +1,17 @@
+let button = document.getElementById('button1')
+console.log(button)
+button.onclick = (e) => {
+  let abc = document.getElementById('con')
+  abc.style.setProperty('display', 'block')
+  setTimeout(() => {
+    abc.style.setProperty('display', 'none')
+  }, 2000)
+}
+
+let image = document.getElementsByClassName('fs-image')
+for(let i = 0; i < image.length; i++){
+  let l = image[i]
+  l.ondblclick = () => {
+  l.requestFullscreen()
+  }
+ }
